a865547e2238b28c112435bbcf36f5e58d007ac7
Andrei Huk <belakolala1@gmail.com>
2022-02-19 03:29:28 +0300

    part_one

--- MyFirstConsole/Program.cs
+++ MyFirstConsole/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using static System.Console;
+class Program
+{
+    static void Main(string[] args)
+    {
+        WriteLine("Hi!");
+        const int Ten = 10;
+        WriteLine($"Our const is {Ten}");
+    Start:
+        WriteLine("If you want to exit the program, please, enter a random char");
+        string userEnter = ReadLine();
+        if (userEnter.Length > 1 || userEnter == " ")
+        {
+            WriteLine("You must enter one char!");
+            goto Start;
+        }
+    }
+}
